2c62a384a0bdacd6f319d87019981c8f9c8b31a5
youxin <youxin@youxins-Mac-mini.local>
2017-07-07 18:15:44 +0800

    yy

--- PandaTV-swift/XMTV/XMTV/Classes/SwiftLernViewController.swift
+++ PandaTV-swift/XMTV/XMTV/Classes/SwiftLernViewController.swift
@@ -8,15 +8,70 @@
 
 import UIKit
 
-class SwiftLernViewController: UIViewController {
+class SwiftLernViewController: UIViewController,UITableViewDelegate,UITableViewDataSource  {
 
+    var tabview:UITableView? = nil;
+    var datasour:NSArray?  = nil;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "swift学习笔记"
         self.view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
+        
+        self.creatData()
+        self.creatUI()
+        
     }
+    func creatData(){
+
+        self.datasour = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+    }
+    
+    func creatUI(){
+        
+       tabview = UITableView.init()
+       tabview?.frame = CGRect(x:0,y:0,width:kScreenW,height:kScreenH);
+        self.view.addSubview(tabview!)
+        
+        tabview?.dataSource = self
+        tabview?.delegate = self
+//        tabview?.register(UITableViewCell.classForCoder(), forCellReuseIdentifier:"cell")
+        
+           tabview?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    
+    
+    }
+    
+  //******************tabviewdeleagte****************
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.datasour!.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell : UITableViewCell = (tabview?.dequeueReusableCell(withIdentifier:"cell"))!
+        cell.backgroundColor = UIColor.green
+        cell.textLabel?.text  = self.datasour?[indexPath.row] as? String
+        
+        return cell
+    }
+  
+    func deselectRow(at indexPath: IndexPath, animated: Bool){
+    
+        let pointVC : PointKnowledgeVC = PointKnowledgeVC()
+        
+        self.navigationController?.pushViewController(pointVC, animated:true)
+    
+    }
+
+  
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
